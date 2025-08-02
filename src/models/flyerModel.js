192d0dfafb40335e96c9d12c5ad4e03bbf4740d8
Md Rayyan Sarfaraz <mdrayyansarfaraz@gmail.com>
2025-08-02 19:33:10 +0530

    updated flyer model

--- src/models/flyerModel.js
+++ src/models/flyerModel.js
@@ -1,20 +1,65 @@
 import mongoose from "mongoose";
+import Event from '@/models/eventModel.js'
+import User from '@/models/userModel.js'
 
 const flyerSchema = new mongoose.Schema({
-    imgUrl:{
+    imgUrl: {
         type: String,
-        required: true ,
-        unique:true
+        required: true,
+        unique: true,
     },
-    description:{
+    description: {
         type: String,
-        required: true
+        required: true,
     },
-    enentId:{
+    eventId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Event",
-        required: true
-    }
+        required: true,
+    },
+
+    orientation: {
+        type: String,
+        enum: ["portrait", "landscape",],
+        default: "portrait",
+    },
+
+    width: { type: Number },
+    height: { type: Number },
+
+    aspectRatio: { type: String },
+
+    displayType: {
+        type: String,
+        enum: ["scroll", "advertisement", "banner", "thumbnail"],
+         default: "scroll",
+    },
+
+
+    views: { type: Number, default: 0 },
+
+    tags: [String],
+    categories: [String],
+
+    likedBy: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }],
+    likesCount: {
+        type: Number,
+        default: 0,
+        min: 0,
+    },
+}, { timestamps: true });
+
+flyerSchema.pre("save", function (next) {
+  if (this.width && this.height && !this.aspectRatio) {
+    const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+    const w = this.width;
+    const h = this.height;
+    const divisor = gcd(w, h);
+    this.aspectRatio = `${w / divisor}:${h / divisor}`;
+  }
 });
 
 const Flyer = mongoose.models.Flyer || mongoose.model("Flyer", flyerSchema);
