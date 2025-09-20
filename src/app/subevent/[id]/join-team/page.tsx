90e0017a3865697142d931ba8f41b0d7a98b8bb7
Md Rayyan Sarfaraz <mdrayyansarfaraz@gmail.com>
2025-09-20 12:13:16 +0530

    bug resolved

--- src/app/subevent/[id]/join-team/page.tsx
+++ src/app/subevent/[id]/join-team/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import JoinTeamForm from "@/components/JoinTeamForm";
 
-"use client";
 
 export default function JoinTeamSubEventPage({ params }: { params: { id: string } }) {
   return <JoinTeamForm type="subevent" id={params.id} />;
